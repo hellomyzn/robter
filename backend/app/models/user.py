f0e308df739640ca3b664365852101c9d488c1d7
hellomyzn <em711th@icloud.com>
2022-10-09 22:12:55 +1100

    test for inserting user data on mysql

---
+++ backend/app/models/user.py
@@ -0,0 +1,34 @@
+import datetime
+
+from sqlalchemy import Column, DateTime, Integer, String, UniqueConstraint, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+import config
+
+
+class Database(object):
+    def __init__(self) -> None:
+        user = config.DB_USER
+        password = config.PASSWORD
+        host = config.HOST
+        db_name = config.DATABASE
+        self.engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{db_name}')
+        self.connect_db()
+
+    def connect_db(self) -> sessionmaker:
+        Base.metadata.create_all(self.engine)
+        session = sessionmaker(self.engine)
+        return session
+
+
+Base = declarative_base()
+database = Database()
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(16))
+    UniqueConstraint(name)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,19 @@ import logging
 import settings
 
 
+
 logging.basicConfig(filename=settings.LOG_FILE, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 if __name__ == '__main__':
     logger.info("running server")
+    from app.models.user import User, database
+    Session = database.connect_db()
+    session = Session()
+    u = User()
+    u.name = "test"
+
+    session.add(u)
+    session.commit()
+    session.close()
